f091a4abc55fa1f79b8f12ecb52e43215df99b04
SilviaSib <Sibonasilvia@gmail.com>
2018-05-28 15:13:28 +0200

    Update1

---
+++ app/src/main/java/ami/amiclean/CleaningRating.java
@@ -0,0 +1,13 @@
+package ami.amiclean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CleaningRating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cleaning_rating);
+    }
+}
